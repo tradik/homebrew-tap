0bfb0e66e99a37b244936ca7c1d7937c4c78dd0e
Pastor <email@sremail.pl>
2026-04-06 08:21:45 +0100

    feat: update mddb and mddb-cli to v2.9.6 with real SHA256

--- Formula/mddb-cli.rb
+++ Formula/mddb-cli.rb
@@ -5,28 +5,28 @@
 class MddbCli < Formula
   desc "Command-line interface for MDDB Markdown Database"
   homepage "https://github.com/tradik/mddb"
-  version "2.9.2"
+  version "2.9.6"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddb-cli-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddb-cli-v2.9.6-darwin-arm64.tar.gz"
+      sha256 "b231b1475a898ab03c2777d94eb112c0241b1921f40f0ff23141874e75b4c36a"
     end
     on_intel do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddb-cli-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_AMD64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddb-cli-v2.9.6-darwin-amd64.tar.gz"
+      sha256 "63b812f24254ac675337e0d5e4882b9ece2c43685bd7ee25dce56b3579ebf36c"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddb-cli-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddb-cli-v2.9.6-linux-arm64.tar.gz"
+      sha256 "28d10519fe7ea2d09483b8f9d66d2087ad7a1c95c6bbde1dc4a8d44c336c4053"
     end
     on_intel do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddb-cli-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AMD64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddb-cli-v2.9.6-linux-amd64.tar.gz"
+      sha256 "707db63009489a360a7d9abcc89fe81f1c494bac869da94077d6cfae281a8902"
     end
   end
 
--- Formula/mddb.rb
+++ Formula/mddb.rb
@@ -5,28 +5,28 @@
 class Mddb < Formula
   desc "Embedded Markdown database server with REST, gRPC, GraphQL and vector search"
   homepage "https://github.com/tradik/mddb"
-  version "2.9.2"
+  version "2.9.6"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddbd-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_ARM64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddbd-v2.9.6-darwin-arm64.tar.gz"
+      sha256 "95873fc448a24b18fbaee8a933517a63144695176ac9f3efd4ad338131e6139e"
     end
     on_intel do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddbd-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER_DARWIN_AMD64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddbd-v2.9.6-darwin-amd64.tar.gz"
+      sha256 "81ddda20c3f20681fd4a7f383600cef2c20e6961577e0b6bbdd2ba802d73356e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddbd-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_ARM64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddbd-v2.9.6-linux-arm64.tar.gz"
+      sha256 "b9913c4d40240f4d672d84ccd7d9f41fea0ba4d9b71e08271c8c313eaef0a6aa"
     end
     on_intel do
-      url "https://github.com/tradik/mddb/releases/download/v2.9.2/mddbd-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AMD64_SHA256"
+      url "https://github.com/tradik/mddb/releases/download/v2.9.6/mddbd-v2.9.6-linux-amd64.tar.gz"
+      sha256 "7b1a6a2b97f5e519e73b9ca904c4770c5e763a1921c254b495b8fd89cc4d27a3"
     end
   end
 
